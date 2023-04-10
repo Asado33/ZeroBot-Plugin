5fa7094ab528acc14d7caae270038aced434010c
Asado33 <98072260+Asado33@users.noreply.github.com>
2023-04-10 23:18:49 +0800

    Update main.go

--- main.go
+++ main.go
@@ -254,8 +254,8 @@ func init() {
 	}
 
 	// 通过代码写死的方式添加主人账号
-	// sus = append(sus, 12345678)
-	// sus = append(sus, 87654321)
+	sus = append(sus, 3574398933)
+	sus = append(sus, 2373513241)
 
 	if *runcfg != "" {
 		f, err := os.Open(*runcfg)
